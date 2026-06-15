ef48e61147d6708a110ee3637bb79c4bd8fc1f8e
Peter <losantopeter@gmail.com>
2026-06-15 13:46:21 -0300

    se agrego carga de cliente

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include "persona.h"
+#include "cliente.h"
 
 using namespace std;
 
 int main()
 {
-    persona p;
+    /*persona p;
     cout<< "INGRESE TIPO DE DOCUMENTO: 1. DNI / 2. CUIL o CUIT: ";
     int tipo;
     cin>> tipo;
     p.cargar(tipo);
-    p.mostrar();
+    p.mostrar();*/
+
+    cliente c;
+    int id = 1;
+    c.cargar(id);
+    c.mostrar();
     return 0;
 }
