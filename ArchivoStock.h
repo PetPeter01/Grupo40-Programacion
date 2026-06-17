bd1e9d9165c80a641b4f6992678a6046fa052de2
Peter <losantopeter@gmail.com>
2026-06-17 00:59:59 -0300

    manejo de stock

---
+++ ArchivoStock.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Stock.h"
+#include "Fecha.h"
+
+class ArchivoStock {
+private:
+    char _nombreArchivo[30];
+    int tamanioRegistro;
+
+public:
+    ArchivoStock(const char* nombre = "Stock.dat");
+
+    int buscarPorIdProducto(int idProducto);
+    Stock leerRegistro(int pos);
+    int agregarRegistro(Stock reg);
+    int modificarRegistro(Stock reg, int pos);
+
+    int getStock(int idProducto);
+    bool hayStockSuficiente(int idProducto, int cantidad);
+    bool sumarStock(int idProducto, int cantidad, Fecha fecha);
+    bool restarStock(int idProducto, int cantidad, Fecha fecha);
+};
+
+
